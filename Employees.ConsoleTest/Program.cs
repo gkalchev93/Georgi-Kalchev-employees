3e02dd67b41a9997ecb57ab6e3fab5d8cfcb1073
gkalchev <gkalchev93@gmail.com>
2018-11-03 16:46:20 +0200

    Created project for test console application

---
+++ Employees.ConsoleTest/Program.cs
@@ -0,0 +1,38 @@
+﻿using Employees.Core.Model;
+using Employees.Core.Utils;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Employees.ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "test";
+
+            //Console.WriteLine("Filepath or file name, if it is in the application folder:");
+            //do
+            //{
+            //    filePath = Console.ReadLine();
+            //} while (!File.Exists(filePath));
+
+            string[] fileLines = File.ReadAllLines(filePath);
+            if (fileLines.Length > 1)
+            {
+                List<EmployeeExp> records = Load.ExpFromLines(fileLines);
+                Console.WriteLine("Records count:" + records.Count);
+                Console.WriteLine();
+
+                var teamWork = Statistics.GetTeamWorkDurations(records);
+                foreach(var work in teamWork)
+                {
+                    Console.WriteLine($"Team {work.Key.Item1}-{work.Key.Item2} has work experiance together {work.Value} days");
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
