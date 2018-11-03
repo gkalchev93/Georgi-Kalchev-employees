7b9966714959171115fbd5823c9bea063d0a433a
gkalchev <gkalchev93@gmail.com>
2018-11-03 16:44:42 +0200

    Imlemented static method ExpFromLines() for loading EmployeeExp from array of lines

---
+++ Employees.Core/Utils/Load.cs
@@ -0,0 +1,44 @@
+﻿using Employees.Core.Model;
+using System;
+using System.Collections.Generic;
+
+namespace Employees.Core.Utils
+{
+    public static class Load
+    {
+        public static List<EmployeeExp> ExpFromLines(string[] lines, char delimiter = ',')
+        {
+            List<EmployeeExp> retCollection = new List<EmployeeExp>();
+
+            foreach (string line in lines)
+            {
+                if (string.IsNullOrEmpty(line))
+                    continue;
+
+                string[] values = line.Split(delimiter);
+                if (values.Length != 4)
+                    continue;
+
+                EmployeeExp tmpObj = new EmployeeExp
+                {
+                    EmpID = int.Parse(values[0]),
+                    ProjectID = int.Parse(values[1]),
+                    DateFrom = DateTime.Parse(values[2])
+                };
+
+                if (DateTime.TryParse(values[3], out DateTime dateToValue))
+                {
+                    tmpObj.DateTo = dateToValue;
+                }
+                else
+                {
+                    tmpObj.DateTo = DateTime.Now;
+                }
+
+                retCollection.Add(tmpObj);
+            }
+
+            return retCollection;
+        }
+    }
+}
