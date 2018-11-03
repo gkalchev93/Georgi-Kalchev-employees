42fe20cee10bd34519b975627a42c0f843d12dfd
gkalchev <gkalchev93@gmail.com>
2018-11-03 16:43:53 +0200

    Created Core Project with EmployeeExp basic model

---
+++ Employees.Core/Model/EmployeeExp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Employees.Core.Model
+{
+    public class EmployeeExp
+    {
+        public int EmpID { get; set; }
+        public int ProjectID { get; set; }
+        public DateTime DateFrom { get; set; }
+        public DateTime DateTo { get; set; }
+    }
+}
